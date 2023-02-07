c8521836a64cac40227e6332a1ae37277e117390
박주원 <devprofessionalism@gmail.com>
2023-02-07 10:36:45 +0900

    JPA - 어플리케이션 개발

--- src/main/java/com/basic/jpa/JpaMain.java
+++ src/main/java/com/basic/jpa/JpaMain.java
@@ -1,8 +1,40 @@
 package com.basic.jpa;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class JpaMain {
 
 	public static void main(String[] args) {
-		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+		EntityManager em = emf.createEntityManager();
+
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+
+		try {
+			// Member findMember = em.find(Member.class, 2L);
+			List<Member> result = em.createQuery("select m from Member as m", Member.class)
+				.setFirstResult(5)
+				.setMaxResults(8)
+				.getResultList();
+
+			for (Member member : result) {
+				System.out.println("member.getName() = " + member.getName());
+			}
+
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.clear();
+		}
+
+		emf.close();
 	}
 }
