b192652b45bfb8ab2dc01d3a9ea4141cc01f3efb
Juwon Park <devprofessionalism@gmail.com>
2023-02-08 21:25:02 +0900

    실전 예제 2 - 연관관계 매핑 시작

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -11,16 +13,18 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class OrerItem {
+public class OrderItem {
 
 	@Id
 	@GeneratedValue
 	@Column(name = "ORDER_ITEM_ID")
 	private Long id;
 
-	@Column(name = "ORDER_ID")
-	private Long orderId;
+	@ManyToOne
+	@JoinColumn(name = "ORDER_ID")
+	private Order order;
 
-	@Column(name = "ITEM_ID")
-	private Long itemId;
+	@ManyToOne
+	@JoinColumn(name = "ITEM_ID")
+	private Item item;
 }
