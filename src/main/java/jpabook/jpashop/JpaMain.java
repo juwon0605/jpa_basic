0d8e135cb83e50d6730098072fd997072a8fc359
Juwon Park <devprofessionalism@gmail.com>
2023-02-11 21:34:56 +0900

    영속성 전이(CASCADE)와 고아 객체

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,14 +1,12 @@
 package jpabook.jpashop;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import jpabook.jpashop.domain.Member;
-import jpabook.jpashop.domain.Team;
+import jpabook.jpashop.domain.Child;
+import jpabook.jpashop.domain.Parent;
 
 public class JpaMain {
 
@@ -21,66 +19,33 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			Team teamA = new Team();
-			teamA.setName("teamA");
-			em.persist(teamA);
+			Child child1 = new Child();
+			Child child2 = new Child();
 
-			Team teamB = new Team();
-			teamA.setName("teamB");
-			em.persist(teamB);
+			Parent parent = new Parent();
+			parent.addChild(child1);
+			parent.addChild(child2);
 
-			Member member1 = new Member();
-			member1.setName("member1");
-			member1.setTeam(teamA);
-			em.persist(member1);
-
-			Member member2 = new Member();
-			member2.setName("member2");
-			member2.setTeam(teamB);
-			em.persist(member2);
+			em.persist(parent);
+			// em.persist(child1); // -> CascadeType.ALL로 생략 가능
+			// em.persist(child2); // -> CascadeType.ALL로 생략 가능
 
 			em.flush();
 			em.clear();
 
-			List<Member> members = em.createQuery("select m from Member m", Member.class)
-				.getResultList();
-			/*
-			Hibernate:
-			select m from Member m
-			select
-				member0_.MEMBER_ID as member_i1_4_,
-				member0_.createBy as createby2_4_,
-				member0_.createDate as createda3_4_,
-				member0_.lastModifiedBy as lastmodi4_4_,
-				member0_.lastModifiedDate as lastmodi5_4_,
-				member0_.city as city6_4_,
-				member0_.name as name7_4_,
-				member0_.street as street8_4_,
-				member0_.TEAM_ID as team_id10_4_,
-				member0_.zipcode as zipcode9_4_
-			from
-				Member member0_
-			 */
-			/*
-			Hibernate:
-				select
-					team0_.id as id1_7_0_,
-					team0_.name as name2_7_0_
-				from
-					Team team0_
-				where
-					team0_.id=?
-			 */
-			/*
-			Hibernate:
-				select
-					team0_.id as id1_7_0_,
-					team0_.name as name2_7_0_
-				from
-					Team team0_
-				where
-					team0_.id=?
-			 */
+			Parent findParent = em.find(Parent.class, parent.getId());
+
+			// Child findChild1 = em.find(Child.class, findParent.getChildList().get(0).getId());
+			// em.remove(findChild1); // -> orphanRemoval = true로 생략 가능
+
+			// findParent.getChildList().remove(0); // ->  orphanRemoval = true로 사용 가능
+
+			// Child findChild2 = em.find(Child.class, findParent.getChildList().get(1).getId());
+			// em.remove(findChild2); // -> orphanRemoval = true
+
+			// findParent.getChildList().remove(1); // ->  orphanRemoval = true로 사용 가능(연관관계의 주인이 아니어도 동작 함)
+
+			em.remove(findParent);
 
 			tx.commit();
 		} catch (Exception e) {
---
+++ src/main/java/jpabook/jpashop/domain/Child.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Child {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "parent_id")
+	private Parent parent;
+}
