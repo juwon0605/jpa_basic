eb12a30816556aea1f98fca267a309af6a1434fa
Juwon Park <devprofessionalism@gmail.com>
2023-02-12 20:03:32 +0900

    값 타입의 비교

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Embeddable;
 
 import lombok.AllArgsConstructor;
@@ -19,4 +21,21 @@ public class Address {
 	private String street;
 
 	private String zipcode;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Address address = (Address)o;
+		return Objects.equals(city, address.city) &&
+			Objects.equals(street, address.street) &&
+			Objects.equals(zipcode, address.zipcode);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(city, street, zipcode);
+	}
 }
